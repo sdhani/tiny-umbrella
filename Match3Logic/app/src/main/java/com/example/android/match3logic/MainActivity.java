f12c68b1936b55a5457d44ba5f9d3bb06dd015ba
shania <shania.dhani46@cuny.edu>
2018-07-29 20:53:57 -0400

    sorted out touch movements and swipe gestures, must find appropriate radius of ImageView to create a substantial ImageView swipe

---
+++ Match3Logic/app/src/main/java/com/example/android/match3logic/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.android.match3logic;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isTouch = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int startX = -1;
+        int startY = -1;
+        int X = (int) event.getX();
+        int Y = (int) event.getY();
+        int eventaction = event.getAction();
+
+        switch (eventaction) {
+            case MotionEvent.ACTION_DOWN:
+                //Toast.makeText(this, "ACTION_DOWN AT COORDS "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+                startX = X;
+                startY = Y;
+                isTouch = true;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                //Toast.makeText(this, "MOVE "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                //Toast.makeText(this, "ACTION_UP "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+
+                if(isTouch && startX < X && startY == Y){
+                    Toast.makeText(this, "RIGHT "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+                }else if(isTouch && startX > X && startY == Y){
+                    Toast.makeText(this, "LEFT "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+                }else if(isTouch && startY < Y && startX == X){
+                    Toast.makeText(this, "DOWN "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+                }else if(isTouch && startY < Y && startX == X){
+                    Toast.makeText(this, "UP "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return true;
+    }
+
+    public static float convertPixelsToDp(float px){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float dp = px / (metrics.densityDpi / 160f);
+        return Math.round(dp);
+    }
+
+    public static float convertDpToPixel(float dp){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return Math.round(px);
+    }
+}
+
+    /*
+    private void touchStart(float x, float y) {
+        myPath = new Path();
+        FingerPath fp = new FingerPath(currentColor, strokeWidth, myPath);
+        paths.add(fp);
+
+        myPath.reset();
+        myPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            myPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void touchUp() {
+        myPath.lineTo(mX, mY);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN :
+                touchStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE :
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP :
+                touchUp();
+                invalidate();
+                break;
+        }
+
+        return true;
+
+*/
+
+
